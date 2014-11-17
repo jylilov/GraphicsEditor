81581f148a40fd9cf8cbd46878749dee21bd8559
Alexander Jylilov <alexander.jylilov@gmail.com>
2014-11-17 14:17:02 +0300

    Add "drawing-mode" action

--- src/graphicseditor.c
+++ src/graphicseditor.c
@@ -6,8 +6,17 @@
 
 struct _GraphicsEditorPrivate {
 	GraphicsEditorWindow *window;
+
 	GSettings *settings;
+	GSimpleAction *drawing_mode;
 };
+
+static void graphicseditor_activate(GApplication *app);
+static void graphicseditor_startup(GApplication *app);
+static void graphicseditor_finalize(GObject *obj);
+static void graphicseditor_set_app_menu(GraphicsEditor *app);
+static void graphicseditor_changed_drawing_mode(GSettings *setting, GVariant *parameter, gpointer user_data);
+static void graphicseditor_change_drawing_mode(GSimpleAction *action, GVariant *parameter, gpointer user_data);
 
 G_DEFINE_TYPE_WITH_PRIVATE(GraphicsEditor, graphicseditor, GTK_TYPE_APPLICATION);
 
@@ -29,15 +38,94 @@ graphicseditor_init (GraphicsEditor *app)
 }
 
 static void
+graphicseditor_class_init (GraphicsEditorClass *class)
+{
+	GApplicationClass *application_class;
+	GObjectClass *object_class;
+
+	application_class = G_APPLICATION_CLASS(class);
+	application_class->activate = graphicseditor_activate;
+	application_class->startup = graphicseditor_startup;
+
+	object_class = G_OBJECT_CLASS(class);
+	object_class->finalize = graphicseditor_finalize;
+}
+
+static void
 graphicseditor_activate (GApplication *app)
 {
-	GraphicsEditorWindow *win;
+	GraphicsEditorPrivate *priv;
+	priv = GRAPHICSEDITOR(app)->priv;
 
-	win = graphicseditor_window_new (GRAPHICSEDITOR (app));
+	priv->window = graphicseditor_window_new (GRAPHICSEDITOR (app));
+	gtk_window_present (GTK_WINDOW (priv->window));
+}
 
-	gtk_window_present (GTK_WINDOW (win));
+static void
+graphicseditor_startup (GApplication *app)
+{
+	GraphicsEditorPrivate *priv;
+	priv = GRAPHICSEDITOR(app)->priv;
 
-	GRAPHICSEDITOR(app)->priv->window = win;
+	G_APPLICATION_CLASS(graphicseditor_parent_class)->startup(app);
+
+	graphicseditor_set_app_menu(GRAPHICSEDITOR(app));
+
+	g_object_set (gtk_settings_get_default (),
+			"gtk-application-prefer-dark-theme", TRUE,
+			NULL);
+}
+
+static void
+graphicseditor_finalize(GObject *obj)
+{
+	GraphicsEditorPrivate *priv;
+	priv = GRAPHICSEDITOR(obj)->priv;
+
+	g_clear_object (&(priv->settings));
+
+	if (G_OBJECT_CLASS (graphicseditor_parent_class)->finalize != NULL)
+		G_OBJECT_CLASS (graphicseditor_parent_class)->finalize(obj);
+}
+
+static void
+graphicseditor_set_app_menu(GraphicsEditor *app)
+{
+	app->priv->drawing_mode = g_simple_action_new_stateful(
+			"drawing-mode",
+			G_VARIANT_TYPE_STRING,
+			g_settings_get_value(app->priv->settings, "drawing-mode"));
+
+	g_signal_connect(app->priv->drawing_mode,
+			"activate",
+			G_CALLBACK(graphicseditor_change_drawing_mode),
+			app);
+
+	g_action_map_add_action_entries(G_ACTION_MAP(app),
+				app_tool_entries, G_N_ELEMENTS(app_tool_entries), app);
+}
+
+static void
+graphicseditor_changed_drawing_mode(GSettings *setting, GVariant *parameter, gpointer user_data)
+{
+	GraphicsEditorPrivate *priv;
+
+	g_return_if_fail(IS_GRAPHICSEDITOR(user_data));
+	priv = GRAPHICSEDITOR(user_data)->priv;
+
+	g_simple_action_set_state(priv->drawing_mode,
+			g_settings_get_value(priv->settings, "drawing-mode"));
+}
+
+static void
+graphicseditor_change_drawing_mode(GSimpleAction *action, GVariant *parameter, gpointer user_data)
+{
+	GraphicsEditorPrivate *priv;
+
+	g_return_if_fail(IS_GRAPHICSEDITOR(user_data));
+	priv = GRAPHICSEDITOR(user_data)->priv;
+
+	g_settings_set_value(priv->settings, "drawing-mode", parameter);
 }
 
 static void
@@ -89,24 +177,6 @@ set_drawing_mode_line_wu(GSimpleAction *action,
 	drawutils_set_drawing_mode(DRAWING_MODE_LINE_WU);
 }
 
-static void
-graphicseditor_startup (GApplication *app)
-{
-	G_APPLICATION_CLASS(graphicseditor_parent_class)->startup(app);
-
-	g_action_map_add_action_entries(G_ACTION_MAP(app),
-			app_tool_entries, G_N_ELEMENTS(app_tool_entries), app);
-
-	g_object_set (gtk_settings_get_default (), "gtk-application-prefer-dark-theme", TRUE, NULL);
-}
-
-static void
-graphicseditor_class_init (GraphicsEditorClass *class)
-{
-	G_APPLICATION_CLASS(class)->activate = graphicseditor_activate;
-	G_APPLICATION_CLASS(class)->startup = graphicseditor_startup;
-}
-
 GraphicsEditor *
 graphicseditor_new (void)
 {
@@ -119,5 +189,11 @@ graphicseditor_new (void)
 			NULL);
 
 	app->priv->settings = g_settings_new("by.jylilov.graphicseditor");
+
+	g_signal_connect(app->priv->settings,
+			"changed::drawing-mode",
+			G_CALLBACK(graphicseditor_changed_drawing_mode),
+			app);
+
 	return app;
 }
